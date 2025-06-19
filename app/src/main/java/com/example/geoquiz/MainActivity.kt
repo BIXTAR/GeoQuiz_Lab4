7ec1f10949ed2dd510b0a5d83da5d7d716b60c8c
BIXTAR <daniil3614@gmail.com>
2025-06-19 22:51:09 +0300

    5. Переработка логики

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.example.geoquiz
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var questionTextView: TextView
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
 
     private val questionBank = listOf(
         Question(R.string.question_1, true),
@@ -12,9 +21,53 @@ class MainActivity : AppCompatActivity() {
     )
 
     private var currentIndex = 0
+    private var correctAnswers = 0
+    private var answeredQuestions = mutableSetOf<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        questionTextView = findViewById(R.id.question_text_view)
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+
+        currentIndex = savedInstanceState?.getInt("index") ?: 0
+        correctAnswers = savedInstanceState?.getInt("correct") ?: 0
+        answeredQuestions = savedInstanceState?.getIntegerArrayList("answered")?.toMutableSet() ?: mutableSetOf()
+
+
+        trueButton.setOnClickListener {
+            checkAnswer(true)
+        }
+
+        falseButton.setOnClickListener {
+            checkAnswer(false)
+        }
+
+        nextButton.setOnClickListener {
+            currentIndex++
+        }
+    }
+
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if (userAnswer == correctAnswer) {
+            correctAnswers++
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+
+        trueButton.isEnabled = false
+        falseButton.isEnabled = false
+        trueButton.visibility = View.INVISIBLE
+        falseButton.visibility = View.INVISIBLE
+
+        answeredQuestions.add(currentIndex)
     }
 }
