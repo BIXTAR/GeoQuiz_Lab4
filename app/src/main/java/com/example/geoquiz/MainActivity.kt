f8a9770b3543cc6c4b375e0667b55625bd6093ab
BIXTAR <daniil3614@gmail.com>
2025-06-19 22:00:26 +0300

    3. Создание списка вопросов, начальная логика

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -4,6 +4,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val questionBank = listOf(
+        Question(R.string.question_1, true),
+        Question(R.string.question_2, false),
+        Question(R.string.question_3, true)
+    )
+
+    private var currentIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
